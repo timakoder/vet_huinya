735c4376243057a400e6ac7524407f3d110496e7
timram <rjckec@gmail.com>
2020-06-03 00:39:47 +0300

    latest updates

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const onLoadCallback = (e: Event) => {
+  console.log(e);
+  console.log('Hello World');
+};
+
+document.addEventListener('DOMContentLoaded', onLoadCallback);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  }
+};
