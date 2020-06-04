cbfa685c469d14d0595a4a9c13b7ff9a8cb672be
timram <rjckec@gmail.com>
2020-06-05 00:32:23 +0300

    latest updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+const makeBodyVisible = () => {
+  const invisbleClass = 'invisible';
+  const allInvisibleItems = document.querySelectorAll(`.${invisbleClass}`);
+  console.log(allInvisibleItems);
+  allInvisibleItems.forEach(i => i.classList.add('visible'));
+}
+
 const onLoadCallback = (e: Event) => {
-  console.log(e);
-  console.log('Hello World');
+  // setTimeout(makeBodyVisible, 1);
 };
 
 document.addEventListener('DOMContentLoaded', onLoadCallback);
