c729f0520afffb220fcff548410921715caabca5
Nickolya01 <nikolay_tihomirov@hotmail.com>
2020-08-03 21:58:15 +0300

    dfgdf

--- js/carusel.js
+++ js/carusel.js
@@ -1,23 +1,209 @@
-/******************************************************************************
-	Transforms the basic Twitter Bootstrap Carousel into Fullscreen Mode
-	@author Fabio Mangolini
-     http://www.responsivewebmobile.com
-******************************************************************************/
-jQuery(document).ready(function() {
-	$('.carousel').carousel({
-    	pause: "true",
-    	interval: 4000
+'use strict';
+
+
+var multiItemSlider = (function () {
+  return function (selector, config) {
+	var
+	  _mainElement = document.querySelector(selector), // основный элемент блока
+	  _sliderWrapper = _mainElement.querySelector('.slider__wrapper'), // обертка для .slider-item
+	  _sliderItems = _mainElement.querySelectorAll('.slider__item'), // элементы (.slider-item)
+	  _sliderControls = _mainElement.querySelectorAll('.slider__control'), // элементы управления
+	  _sliderControlLeft = _mainElement.querySelector('.slider__control_left'), // кнопка "LEFT"
+	  _sliderControlRight = _mainElement.querySelector('.slider__control_right'), // кнопка "RIGHT"
+	  _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), // ширина обёртки
+	  _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), // ширина одного элемента    
+	  _positionLeftItem = 0, // позиция левого активного элемента
+	  _transform = 0, // значение транфсофрмации .slider_wrapper
+	  _step = _itemWidth / _wrapperWidth * 100, // величина шага (для трансформации)
+	  _items = []; // массив элементов
+	// наполнение массива _items
+	_sliderItems.forEach(function (item, index) {
+	  _items.push({ item: item, position: index, transform: 0 });
 	});
 
-	$('.carousel').css({'margin': 0, 'width': $(window).outerWidth(), 'height': $(window).outerHeight()});
-	$('.carousel .item').css({'position': 'fixed', 'width': '100%', 'height': '100%'});
-	$('.carousel-inner div.item img').each(function() {
-		var imgSrc = $(this).attr('src');
-		$(this).parent().css({'background': 'url('+imgSrc+') center center no-repeat', '-webkit-background-size': '100% ', '-moz-background-size': '100%', '-o-background-size': '100%', 'background-size': '100%', '-webkit-background-size': 'cover', '-moz-background-size': 'cover', '-o-background-size': 'cover', 'background-size': 'cover'});
-		$(this).remove();
-	});
+	var position = {
+	  getMin: 0,
+	  getMax: _items.length - 1,
+	}
 
-	$(window).on('resize', function() {
-		$('.carousel').css({'width': $(window).outerWidth(), 'height': $(window).outerHeight()});
-	});
-}); 
+	var _transformItem = function (direction) {
+	  if (direction === 'right') {
+		if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) >= position.getMax) {
+		  return;
+		}
+		if (!_sliderControlLeft.classList.contains('slider__control_show')) {
+		  _sliderControlLeft.classList.add('slider__control_show');
+		}
+		if (_sliderControlRight.classList.contains('slider__control_show') && (_positionLeftItem + _wrapperWidth / _itemWidth) >= position.getMax) {
+		  _sliderControlRight.classList.remove('slider__control_show');
+		}
+		_positionLeftItem++;
+		_transform -= _step;
+	  }
+	  if (direction === 'left') {
+		
+
+		if (_positionLeftItem <= position.getMin) {
+		  return;
+		}
+		if (!_sliderControlRight.classList.contains('slider__control_show')) {
+		  _sliderControlRight.classList.add('slider__control_show');
+		}
+		if (_sliderControlLeft.classList.contains('slider__control_show') && _positionLeftItem - 1 <= position.getMin) {
+		  _sliderControlLeft.classList.remove('slider__control_show');
+		}
+		_positionLeftItem--;
+		_transform += _step;
+	  }
+	  _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+	}
+
+	
+
+	// обработчик события click для кнопок "назад" и "вперед"
+	var _controlClick = function (e) {
+	  if (e.target.classList.contains('slider__control')) {
+		e.preventDefault();
+		var direction = e.target.classList.contains('slider__control_right') ? 'right' : 'left';
+		_transformItem(direction);
+	  }
+	};
+
+	var _setUpListeners = function () {
+	  // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
+	  _sliderControls.forEach(function (item) {
+		item.addEventListener('click', _controlClick);
+	  });
+	}
+
+	// инициализация
+	_setUpListeners();
+
+	return {
+	  right: function () { // метод right
+		_transformItem('right');
+	  },
+	  left: function () { // метод left
+		_transformItem('left');
+	  }
+	}
+
+  }
+}());
+
+var slider = multiItemSlider('.slider')
+
+
+
+
+
+
+
+'use strict';
+    var multiItemSlider = (function () {
+      return function (selector, config) {
+        var
+          _mainElement = document.querySelector(selector), // основный элемент блока
+          _sliderWrapper1 = _mainElement.querySelector('.slider__wrapper1'), // обертка для .slider-item
+          _sliderItems = _mainElement.querySelectorAll('.slider__item1'), // элементы (.slider-item)
+          _sliderControls = _mainElement.querySelectorAll('.slider__control1'), // элементы управления
+          _sliderControlLeft = _mainElement.querySelector('.slider__control_left1'), // кнопка "LEFT"
+          _sliderControlRight = _mainElement.querySelector('.slider__control_right1'), // кнопка "RIGHT"
+          _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper1).width), // ширина обёртки
+          _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), // ширина одного элемента    
+          _positionLeftItem = 0, // позиция левого активного элемента
+          _transform = 0, // значение транфсофрмации .slider_wrapper
+          _step = _itemWidth / _wrapperWidth * 100, // величина шага (для трансформации)
+          _items = []; // массив элементов
+
+        // наполнение массива _items
+        _sliderItems.forEach(function (item, index) {
+          _items.push({ item: item, position: index, transform: 0 });
+        });
+
+        var position = {
+          getItemMin: function () {
+            var indexItem = 0;
+            _items.forEach(function (item, index) {
+              if (item.position < _items[indexItem].position) {
+                indexItem = index;
+              }
+            });
+            return indexItem;
+          },
+          getItemMax: function () {
+            var indexItem = 0;
+            _items.forEach(function (item, index) {
+              if (item.position > _items[indexItem].position) {
+                indexItem = index;
+              }
+            });
+            return indexItem;
+          },
+          getMin: function () {
+            return _items[position.getItemMin()].position;
+          },
+          getMax: function () {
+            return _items[position.getItemMax()].position;
+          }
+        }
+
+        var _transformItem = function (direction) {
+          var nextItem;
+          if (direction === 'right') {
+            _positionLeftItem++;
+            if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) > position.getMax()) {
+              nextItem = position.getItemMin();
+              _items[nextItem].position = position.getMax() + 1;
+              _items[nextItem].transform += _items.length * 100;
+              _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+            }
+            _transform -= _step;
+          }
+          if (direction === 'left') {
+            _positionLeftItem--;
+            if (_positionLeftItem < position.getMin()) {
+              nextItem = position.getItemMax();
+              _items[nextItem].position = position.getMin() - 1;
+              _items[nextItem].transform -= _items.length * 100;
+              _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+            }
+            _transform += _step;
+          }
+          _sliderWrapper1.style.transform = 'translateX(' + _transform + '%)';
+        }
+
+        // обработчик события click для кнопок "назад" и "вперед"
+        var _controlClick = function (e) {
+          if (e.target.classList.contains('slider__control1')) {
+            e.preventDefault();
+            var direction = e.target.classList.contains('slider__control_right1') ? 'right' : 'left';
+            _transformItem(direction);
+          }
+        };
+
+        var _setUpListeners = function () {
+          // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
+          _sliderControls.forEach(function (item) {
+            item.addEventListener('click', _controlClick);
+          });
+        }
+
+        // инициализация
+        _setUpListeners();
+
+        return {
+          right: function () { // метод right
+            _transformItem('right');
+          },
+          left: function () { // метод left
+            _transformItem('left');
+          }
+        }
+
+      }
+    }());
+
+    var slider = multiItemSlider('.slider1')
+
+
---
+++ js/slider.js
@@ -0,0 +1,157 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var CitiesSlider = function (_React$Component) {
+  _inherits(CitiesSlider, _React$Component);
+
+  function CitiesSlider(props) {
+    _classCallCheck(this, CitiesSlider);
+
+    var _this = _possibleConstructorReturn(this, _React$Component.call(this, props));
+
+    _this.IMAGE_PARTS = 4;
+
+    _this.changeTO = null;
+    _this.AUTOCHANGE_TIME = 4000;
+
+    _this.state = { activeSlide: -1, prevSlide: -1, sliderReady: false };
+    return _this;
+  }
+
+  CitiesSlider.prototype.componentWillUnmount = function componentWillUnmount() {
+    window.clearTimeout(this.changeTO);
+  };
+
+  CitiesSlider.prototype.componentDidMount = function componentDidMount() {
+    var _this2 = this;
+
+    this.runAutochangeTO();
+    setTimeout(function () {
+      _this2.setState({ activeSlide: 0, sliderReady: true });
+    }, 0);
+  };
+
+  CitiesSlider.prototype.runAutochangeTO = function runAutochangeTO() {
+    var _this3 = this;
+
+    this.changeTO = setTimeout(function () {
+      _this3.changeSlides(1);
+      _this3.runAutochangeTO();
+    }, this.AUTOCHANGE_TIME);
+  };
+
+  CitiesSlider.prototype.changeSlides = function changeSlides(change) {
+    window.clearTimeout(this.changeTO);
+    var length = this.props.slides.length;
+
+    var prevSlide = this.state.activeSlide;
+    var activeSlide = prevSlide + change;
+    if (activeSlide < 0) activeSlide = length - 1;
+    if (activeSlide >= length) activeSlide = 0;
+    this.setState({ activeSlide: activeSlide, prevSlide: prevSlide });
+  };
+
+  CitiesSlider.prototype.render = function render() {
+    var _this4 = this;
+
+    var _state = this.state;
+    var activeSlide = _state.activeSlide;
+    var prevSlide = _state.prevSlide;
+    var sliderReady = _state.sliderReady;
+
+    return React.createElement(
+      'div',
+      { className: classNames('slider', { 's--ready': sliderReady }) },
+      React.createElement(
+        'p',
+        { className: 'slider__top-heading' },
+        'Travelers'
+      ),
+      React.createElement(
+        'div',
+        { className: 'slider__slides' },
+        this.props.slides.map(function (slide, index) {
+          return React.createElement(
+            'div',
+            {
+              className: classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index }),
+              key: slide.city
+            },
+            React.createElement(
+              'div',
+              { className: 'slider__slide-content' },
+              React.createElement(
+                'h3',
+                { className: 'slider__slide-subheading' },
+                slide.country || slide.city
+              ),
+              React.createElement(
+                'h2',
+                { className: 'slider__slide-heading' },
+                slide.city.split('').map(function (l) {
+                  return React.createElement(
+                    'span',
+                    null,
+                    l
+                  );
+                })
+              ),
+              React.createElement(
+                'p',
+                { className: 'slider__slide-readmore' },
+                'read more'
+              )
+            ),
+            React.createElement(
+              'div',
+              { className: 'slider__slide-parts' },
+              [].concat(Array(_this4.IMAGE_PARTS).fill()).map(function (x, i) {
+                return React.createElement(
+                  'div',
+                  { className: 'slider__slide-part', key: i },
+                  React.createElement('div', { className: 'slider__slide-part-inner', style: { backgroundImage: 'url(' + slide.img + ')' } })
+                );
+              })
+            )
+          );
+        })
+      ),
+      React.createElement('div', { className: 'slider__control', onClick: function onClick() {
+          return _this4.changeSlides(-1);
+        } }),
+      React.createElement('div', { className: 'slider__control slider__control--right', onClick: function onClick() {
+          return _this4.changeSlides(1);
+        } })
+    );
+  };
+
+  return CitiesSlider;
+}(React.Component);
+
+var slides = [{
+  city: 'Paris',
+  country: 'France',
+  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg'
+}, {
+  city: 'Singapore',
+  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg'
+}, {
+  city: 'Prague',
+  country: 'Czech Republic',
+  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg'
+}, {
+  city: 'Amsterdam',
+  country: 'Netherlands',
+  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg'
+}, {
+  city: 'Moscow',
+  country: 'Russia',
+  img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg'
+}];
+
+ReactDOM.render(React.createElement(CitiesSlider, { slides: slides }), document.querySelector('#app'));
